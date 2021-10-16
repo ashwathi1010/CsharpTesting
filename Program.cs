deabfef6102eeb73a6c0abd9ee9b52d0cba739ba
Ashwathi <ashwathi1010@gmail.com>
2021-10-16 18:01:35 -0400

    Datatype exercises

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DTexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter1, letter2, letter3;
+            Console.WriteLine("Enter Letter1: ");
+            letter1 = Convert.ToChar
+            Console.WriteLine("Enter Letter2: ");
+            Console.WriteLine("Enter Letter3: ");
+
+        }
+    }
+}
